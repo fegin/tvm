1862108ac804a229c45960c98f7ecd255d4f062b
fegin <fegins@gmail.com>
2019-04-11 21:42:05 -0400

    Fix the issue that SA_LoadGraph doesn't create all swapin nodes correctly. The previous implementation miss the create of update nodes. This implementation create the update nodes but doesn't connect them (TODO).

--- nnvm/src/pass/sa_load_graph.cc
+++ nnvm/src/pass/sa_load_graph.cc
@@ -82,6 +82,7 @@ void CreateSwapout(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
                    std::unordered_map<uint32_t, NodeEntry>& swapouts) {
   std::cout << "CreateSwapout" << std::endl;
   for (const auto& kv: sa_nodes) {
+    if (kv.second.name != "swapout") continue;
     NodePtr node = Node::Create();
     node->attrs.op = swapout_op;
     node->attrs.name = "swapout";
@@ -98,12 +99,30 @@ void CreateSwapin(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
                   std::unordered_map<uint32_t, NodeEntry>& swapins) {
   std::cout << "CreateSwapin" << std::endl;
   for (const auto& kv: sa_nodes) {
+    if (kv.second.name != "swapin") continue;
     NodePtr node = Node::Create();
     node->attrs.op = swapin_op;
-    node->attrs.name = "swapout";
+    node->attrs.name = "swapin";
     //node->attrs.op->attr_parser(&(node->attrs));
     node->control_deps.emplace_back(swap_entry.node);
     swapins[kv.first] = NodeEntry{std::move(node), 0, 0};
+  }
+}
+
+void CreateUpdate(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
+                  const NodeEntry& swapout_sink,
+                  const Op* update_op,
+                  std::unordered_map<uint32_t, NodeEntry>& updates) {
+  std::cout << "CreateUpdate" << std::endl;
+  for (const auto& kv: sa_nodes) {
+    if (kv.second.name.find("weight_g_update") == std::string::npos) continue;
+    NodePtr node = Node::Create();
+    node->attrs.op = update_op;
+    node->attrs.name = kv.second.name;
+    //node->attrs.op->attr_parser(&(node->attrs));
+    // FIXME(fegin)
+    //swapout_sink.node->control_deps.emplace_back(node);
+    updates[kv.first] = NodeEntry{std::move(node), 0, 0};
   }
 }
 
@@ -174,6 +193,59 @@ void CreateModelNodes(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
   }
 }
 
+void ConnectSwapout(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
+                    const IndexedGraph& idx,
+                    std::unordered_map<uint32_t, NodeEntry>& swapouts,
+                    std::unordered_map<uint32_t, NodeEntry>& swapins,
+                    std::unordered_map<uint32_t, NodeEntry>& updates,
+                    std::unordered_map<uint32_t, NodeEntry>& variables,
+                    std::unordered_map<uint32_t, NodePtr>& new_nodes) {
+  std::cout << "ConnectAllSwapout" << std::endl;
+  for (auto& kv: swapouts) {
+    uint32_t sa_nid = kv.first;
+    if (sa_nodes.at(sa_nid).deps.size() == 0) continue;
+    NodeEntry& entry = kv.second;
+    for (uint32_t dep_nid : sa_nodes.at(sa_nid).deps) {
+      // Depend on another swapout.
+      auto so_it = swapouts.find(dep_nid);
+      if (so_it != swapouts.end()) {
+        entry.node->control_deps.emplace_back(so_it->second.node);
+        continue;
+      }
+
+      // Depend on a swapin.
+      auto si_it = swapins.find(dep_nid);
+      if (si_it != swapins.end()) {
+        entry.node->control_deps.emplace_back(si_it->second.node);
+        continue;
+      }
+
+      // Depend on a variable.
+      auto var_it = variables.find(dep_nid);
+      if (var_it != variables.end()) {
+        entry.node->control_deps.emplace_back(var_it->second.node);
+        continue;
+      }
+
+      // Depend on a update node.
+      auto update_it = updates.find(dep_nid);
+      if (update_it != updates.end()) {
+        // FIXME(fegin):
+        //entry.node->control_deps.emplace_back(node_it->second);
+        continue;
+      }
+
+      // Depend on a model node.
+      auto node_it = new_nodes.find(dep_nid);
+      if (node_it != new_nodes.end()) {
+        entry.node->control_deps.emplace_back(node_it->second);
+        continue;
+      }
+      CHECK(false) << dep_nid  << ", " << new_nodes.size();
+    }
+  }
+}
+
 void ConnectPreSwapin(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
                       const IndexedGraph& idx,
                       std::unordered_map<uint32_t, NodeEntry>& swapins,
@@ -184,6 +256,7 @@ void ConnectPreSwapin(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
     if (sa_nodes.at(sa_nid).deps.size() > 0) continue;
     NodeEntry& entry = kv.second;
     for (const auto var_id : sa_nodes.at(sa_nid).be_depended) {
+      std::cout << "DEPENDS on PRESWAPIN" << std::endl;
       variables.at(var_id).node->control_deps.emplace_back(entry.node);
     }
   }
@@ -232,25 +305,20 @@ void ConnectAllSwapin(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
   }
 }
 
-void ConnectSwapout(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
-                    const IndexedGraph& idx,
-                    std::unordered_map<uint32_t, NodeEntry>& swapouts,
-                    std::unordered_map<uint32_t, NodeEntry>& swapins,
-                    std::unordered_map<uint32_t, NodeEntry>& variables,
-                    std::unordered_map<uint32_t, NodePtr>& new_nodes) {
-  std::cout << "ConnectAllSwapout" << std::endl;
-  for (auto& kv: swapouts) {
+void ConnectUpdate(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
+                   const IndexedGraph& idx,
+                   std::unordered_map<uint32_t, NodeEntry>& updates,
+                   std::unordered_map<uint32_t, NodeEntry>& swapins,
+                   std::unordered_map<uint32_t, NodeEntry>& variables,
+                   std::unordered_map<uint32_t, NodePtr>& new_nodes) {
+  // FIXME(fegin):
+  return;
+  std::cout << "ConnectUpdate" << std::endl;
+  for (auto& kv: updates) {
     uint32_t sa_nid = kv.first;
     if (sa_nodes.at(sa_nid).deps.size() == 0) continue;
     NodeEntry& entry = kv.second;
     for (uint32_t dep_nid : sa_nodes.at(sa_nid).deps) {
-      // Depend on another swapout.
-      auto so_it = swapouts.find(dep_nid);
-      if (so_it != swapouts.end()) {
-        entry.node->control_deps.emplace_back(so_it->second.node);
-        continue;
-      }
-
       // Depend on a swapin.
       auto si_it = swapins.find(dep_nid);
       if (si_it != swapins.end()) {
@@ -269,12 +337,12 @@ void ConnectSwapout(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
       auto node_it = new_nodes.find(dep_nid);
       if (node_it != new_nodes.end()) {
         entry.node->control_deps.emplace_back(node_it->second);
+        continue;
       }
-      CHECK(false);
+      CHECK(false) << dep_nid  << ", " << new_nodes.size();
     }
   }
 }
-
 void ConnectModelNodes(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
                        const IndexedGraph& idx,
                        std::unordered_map<uint32_t, NodePtr>& new_nodes,
@@ -327,17 +395,22 @@ void ConnectModelNodes(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
       }
     }
 
-    std::cout << "ConnectModeNode new_deps" << deps.size() << std::endl;
+    std::cout << "ConnectModeNode node " << sa_nid << " with " << deps.size()
+              << std::endl;
+    std::cout << std::endl;
     for (uint32_t dep_nid : deps) {
       // Depend on another model node.
       auto node_it = new_nodes.find(dep_nid);
       if (node_it != new_nodes.end()) {
+        std::cout << "DEPENDS on another model node." << std::endl;
         new_node->control_deps.emplace_back(node_it->second);
+        continue;
       }
 
       // Depend on a swapout.
       auto so_it = swapouts.find(dep_nid);
       if (so_it != swapouts.end()) {
+        std::cout << "DEPENDS on SWAPOUT" << std::endl;
         new_node->control_deps.emplace_back(so_it->second.node);
         continue;
       }
@@ -345,6 +418,7 @@ void ConnectModelNodes(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
       // Depend on a swapin.
       auto si_it = swapins.find(dep_nid);
       if (si_it != swapins.end()) {
+        std::cout << "DEPENDS on SWAPIN" << std::endl;
         new_node->control_deps.emplace_back(si_it->second.node);
         continue;
       }
@@ -352,11 +426,12 @@ void ConnectModelNodes(const std::unordered_map<uint32_t, SA_Node>& sa_nodes,
       // Depend on a variable.
       auto var_it = variables.find(dep_nid);
       if (var_it != variables.end()) {
+        std::cout << "DEPENDS on a variable." << std::endl;
         new_node->control_deps.emplace_back(var_it->second.node);
         continue;
       }
 
-      CHECK(false);
+      CHECK(false) << dep_nid;
     }
   }
 }
@@ -375,9 +450,10 @@ Graph SA_LoadGraph(Graph src) {
   std::unordered_map<Node*, uint32_t> nodeptr_to_old_nid;
   std::unordered_map<uint32_t, NodeEntry> swapouts;       // SA_ID -> swapout NodeEntry
   std::unordered_map<uint32_t, NodeEntry> swapins;        // SA_ID -> swapin NodeEntry
-  std::unordered_map<uint32_t, SA_Node> sa_nodes;         // SA_ID -> SA_Node
+  std::unordered_map<uint32_t, NodeEntry> updates;        // SA_ID -> update NodeEntry
   std::unordered_map<uint32_t, NodeEntry> variables;      // SA_ID -> new variable NodeEntry
   std::unordered_map<uint32_t, NodePtr> new_nodes;        // SA_ID -> new model NodeEntry
+  std::unordered_map<uint32_t, SA_Node> sa_nodes;         // SA_ID -> SA_Node
 
   // Create all the new swapout, swapin and nodes.
   // Connect all of them together.
@@ -386,11 +462,15 @@ Graph SA_LoadGraph(Graph src) {
   NodeEntry swapout_sink = CreateSwapoutSink(swapout_sink_op);
   CreateSwapout(sa_nodes, swap_entry, swapout_sink, swapout_op, swapouts);
   CreateSwapin(sa_nodes, swap_entry, swapin_op, swapins);
+  // FIXME(fegin):
+  CreateUpdate(sa_nodes, swapout_sink, NULL, updates);
   CreateVariables(sa_nodes, idx, swap_entry, variables, nodeptr_to_old_nid);
   CreateModelNodes(sa_nodes, idx, new_nodes, nodeptr_to_old_nid);
+  ConnectSwapout(sa_nodes, idx, swapouts, swapins, updates, variables,
+                 new_nodes);
   ConnectPreSwapin(sa_nodes, idx, swapins, variables);
   ConnectAllSwapin(sa_nodes, idx, swapins, swapouts, variables, new_nodes);
-  ConnectSwapout(sa_nodes, idx, swapouts, swapins, variables, new_nodes);
+  ConnectUpdate(sa_nodes, idx, updates, swapins, variables, new_nodes);
   ConnectModelNodes(sa_nodes, idx, new_nodes, swapouts, swapins, variables);
 
   // Create a new graph
